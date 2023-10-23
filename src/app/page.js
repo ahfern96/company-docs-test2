97405a599796686acad8812ced256e71372417b2
ahfern96 <116745227+ahfern96@users.noreply.github.com>
2023-10-23 20:01:29 +0000

    fixed svg load

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,13 @@
+"use client"
+
 import Image from 'next/image'
 import styles from './page.module.css'
-import Logo from "./diagram1.svg"
+import Diagram1 from "./diagram1.svg"
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Hello
-      {/* <Image 
-        src="/diagram1.svg"
-        width={500}
-        height={500}
-        data="/diagram1.svg"
-      /> */}
-      <Logo/>
+      <Diagram1/>
     </main>
   )
 }
