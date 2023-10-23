1f0987217466fc8ce157c0b0a393253c1a6e5def
ahfern96 <116745227+ahfern96@users.noreply.github.com>
2023-10-23 19:40:53 +0000

    trying to modify webpack

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Logo from "./diagram1.svg"
 
 export default function Home() {
   return (
     <main className={styles.main}>
       Hello
+      {/* <Image 
+        src="/diagram1.svg"
+        width={500}
+        height={500}
+        data="/diagram1.svg"
+      /> */}
+      <Logo/>
     </main>
   )
 }
