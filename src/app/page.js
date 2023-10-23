c945711aa7587328b068fc5adac5faafce7061ee
ahfern96 <116745227+ahfern96@users.noreply.github.com>
2023-10-23 20:59:34 +0000

    EoD

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,11 @@ import styles from './page.module.css'
 import Diagram1 from "./diagram1.svg"
 
 export default function Home() {
+
   return (
     <main className={styles.main}>
       <Diagram1/>
+      <a href="https://mdxjs.com/docs/what-is-mdx/">MDX</a>
     </main>
   )
 }
