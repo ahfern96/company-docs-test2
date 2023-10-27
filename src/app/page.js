6196ff4aacc7e4fdc9a4cc2e94f9b814d85438da
ahfern96 <116745227+ahfern96@users.noreply.github.com>
2023-10-27 12:46:05 +0000

    mdx routes working

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Diagram1/><br/>
-      <a href="https://mdxjs.com/docs/what-is-mdx/">MDX</a><br/>
-      <a href="https://youtu.be/n2CV6f0vFr4?t=386">MDX next with app directory</a>
     </main>
   )
 }
