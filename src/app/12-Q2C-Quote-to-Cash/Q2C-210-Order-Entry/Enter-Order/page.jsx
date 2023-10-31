d70ba40860f3297ad9443b6bd2ed2fe665731339
ahfern96 <116745227+ahfern96@users.noreply.github.com>
2023-10-31 18:29:48 +0000

    working2

--- src/app/12-Q2C-Quote-to-Cash/Q2C-210-Order-Entry/Enter-Order/page.jsx
+++ src/app/12-Q2C-Quote-to-Cash/Q2C-210-Order-Entry/Enter-Order/page.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import Image from 'next/image'
+import Diagram1 from './AAA-interactive-test.svg'
 
 
 export default function Page(){
@@ -19,7 +20,9 @@ export default function Page(){
                 <li>6. Verify Release(s) Details</li>
                 <li>6. Need Additional line(s) information</li>
             </ol>
-            <img src={"/select-site1.png"} />
+            {/* <img src={"/select-site1.png"} /> */}
+            <Diagram1/>
+            <iframe src="https://ferncous-my.sharepoint.com/personal/aiden_hoffman_fernco_com/_layouts/15/embed.aspx?UniqueId=eb134653-3d8b-4030-bec2-925114d27859&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen title="select-site.mp4"></iframe>
         </main>
     )
 }
