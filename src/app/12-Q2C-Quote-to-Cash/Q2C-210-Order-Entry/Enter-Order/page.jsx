dd15ca6668ae1ce6a4d40c784a8b34be2482115d
ahfern96 <116745227+ahfern96@users.noreply.github.com>
2023-10-31 18:32:28 +0000

    working 4

--- src/app/12-Q2C-Quote-to-Cash/Q2C-210-Order-Entry/Enter-Order/page.jsx
+++ src/app/12-Q2C-Quote-to-Cash/Q2C-210-Order-Entry/Enter-Order/page.jsx
@@ -21,8 +21,8 @@ export default function Page(){
                 <li>6. Need Additional line(s) information</li>
             </ol>
             {/* <img src={"/select-site1.png"} /> */}
-            <Diagram1/>
-            <iframe src="https://ferncous-my.sharepoint.com/personal/aiden_hoffman_fernco_com/_layouts/15/embed.aspx?UniqueId=eb134653-3d8b-4030-bec2-925114d27859&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen title="select-site.mp4"></iframe>
+            {/* <Diagram1/> */}
+            {/* <iframe src="https://ferncous-my.sharepoint.com/personal/aiden_hoffman_fernco_com/_layouts/15/embed.aspx?UniqueId=eb134653-3d8b-4030-bec2-925114d27859&embed=%7B%22ust%22%3Atrue%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen title="select-site.mp4"></iframe> */}
         </main>
     )
 }
